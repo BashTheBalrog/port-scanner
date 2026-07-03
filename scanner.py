b4134f91831e3cc77a60c4281bf68796a97ed81d
BashTheBalrog <hmistry848@gmail.com>
2026-07-03 14:35:30 +0530

    slow stack based scanner with option to scan individual/multiple ports

--- scanner.py
+++ scanner.py
@@ -7,5 +7,27 @@ print(target)
 ip=socket.gethostbyname(target)
 print("IP: ")
 print(ip)
+print("Scan one port => 1 OR Scan ALL PORTS => 2")
+choice=int(input("Enter your choice: "))
 sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.settimeout(1)
 print(sock)
+if choice==1:
+    port=int(input("Enter port number to scan: "))
+    result=sock.connect_ex((ip,port))
+    print(result)
+
+    if result==0:
+        print(f"Port {port} is OPEN")
+    else:
+        print(f"Port {port} is CLOSED")
+elif choice==2:
+    for port in range(1,65535):
+        sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.settimeout(0.05)
+        result=sock.connect_ex((ip,port))
+        if result==0:
+            print(f"Port {port} is OPEN")
+        #else:
+           # print(f"Port {port} is CLOSED")
+sock.close()
